104442f9edffb064621c69f1ee5035c358c399ff
Mark Pieter Hendrik Raadsen <mark.raadsen@sydney.edu.au>
2018-05-18 14:23:29 +1000

    Test

---
+++ src/org/planit/core/network/Node.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.planit.core.network;
+
+//import java.awt.geom.Point2D;
+
+/**
+ * @author markr
+ *
+ */
+public class Node {
+	
+	// replace Point2D with ARCGIS geometry point, see word doc on this
+	//private Point2D xyCoord;
+
+
+
+}
