7c0e83190a171037b2be61c260a7703e476f44a7
Mark Raadsen <mark.raadsen@sydney.edu.au>
2019-05-06 10:01:28 +1000

    Removed redundant ids of mode and traveller type as well as the id based constructor, which is not allowed (any reader who not yet has mode and traveller type when creating the user class should do this first internally, not go through the PALNit core functionality)

--- src/main/java/org/planit/userclass/UserClass.java
+++ src/main/java/org/planit/userclass/UserClass.java
@@ -6,7 +6,8 @@ import java.util.logging.Logger;
 import org.planit.utils.IdGenerator;
 
 /** A user class defines a combination of one or more characteristics of users in an aggregate representation of traffic
- * which partially dictate how they behave in traffic assignment. 
+ * which partially dictate how they behave in traffic assignment.
+ *  
  * @author markr
  *
  */
@@ -17,48 +18,39 @@ public class UserClass {
      */
     private static final Logger LOGGER = Logger.getLogger(UserClass.class.getName());
         
- /**
-  * id of this user class
-  */
+     /**
+      * id of this user class
+      */
 	private long id;
 	
-/**
- * Name of this user class
- */
+    /**
+     * Name of this user class
+     */
 	private String name;
 	
-/**
- * Mode of travel of this user class
- */
+    /**
+     * Mode of travel of this user class
+     */
 	private Mode mode;
 	
-/**
- * Traveller type of this user class
- */
+	/**
+	 * Traveller type of this user class
+	 */
 	private TravellerType travellerType;
 	
-/**
- * Id of mode of travel
- */
-	private long modeId = 0;
-	
-/**
- * Id of this traveller type
- */
-	private long travellerTypeId = 0;
-	
-/**
- * Map to store registered user classes
- */
+
+    /**
+     * Map to store registered user classes
+     */
 	private static Map<Long, UserClass> userClasses = new HashMap<Long, UserClass>();
 	
-/**
- * Constructor of user class
- * 
- * @param name                 the name of this user class
- * @param mode                 the mode of travel
- * @param travellerType      the traveller type
- */
+    /**
+     * Constructor of user class
+     * 
+     * @param name                 the name of this user class
+     * @param mode                 the mode of travel
+     * @param travellerType      the traveller type
+     */
 	public UserClass(String name, Mode mode, TravellerType travellerType) {
 		this.id = IdGenerator.generateId(UserClass.class);
 		this.name = name;
@@ -67,14 +59,14 @@ public class UserClass {
 		userClasses.put(this.id, this);
 	}
 	
-/**
- * Constructor of user class
- * 
- * @param id                       id of this user class
- * @param name                 the name of this user class
- * @param mode                 the mode of travel
- * @param travellerType      the traveller type
- */
+    /**
+     * Constructor of user class
+     * 
+     * @param id                       id of this user class
+     * @param name                 the name of this user class
+     * @param mode                 the mode of travel
+     * @param travellerType      the traveller type
+     */
 	public UserClass(long id, String name, Mode mode, TravellerType travellerType) {
 		this.id = id;
 		this.name = name;
@@ -83,31 +75,13 @@ public class UserClass {
 		userClasses.put(this.id, this);
 	}
 	
-/**
- * Constructor of user class
- * 
- * @param id                       id of this user class
- * @param name                name of this user class
- * @param modeId             id of mode of travel
- * @param travellerTypeId  if of traveller type
- */
-	public UserClass(long id, String name, long modeId, long travellerTypeId) {
-		this.id = id;
-		this.name = name;
-		this.modeId = modeId;
-		this.travellerTypeId = travellerTypeId;
-		this.mode = Mode.getById(modeId);
-		this.travellerType = TravellerType.getById(travellerTypeId);
-		userClasses.put(this.id, this);
-	}
-	
-/**
- * Retrieve user class by id
- * 
- * @param id        id of user class to be retrieve
- * @return             retrieved user class	
- */
-    public static UserClass getById(long id) {
+    /**
+     * Retrieve user class by id
+     * 
+     * @param id        id of user class to be retrieve
+     * @return             retrieved user class	
+     */
+    public static UserClass getUserClass(long id) {
     	return userClasses.get(id);
     }
 	
@@ -116,62 +90,44 @@ public class UserClass {
    * 
    * @param userClass       the user class to be stored
    */
-	public static void putById(UserClass userClass) {
+	public static void registerById(UserClass userClass) {
 		userClasses.put(userClass.getId(), userClass);
 	}
 	
-/**
- * Get the traveller type of this user class
- * 
- * @return       TravellerType of this user class
- */
+    /**
+     * Get the traveller type of this user class
+     * 
+     * @return       TravellerType of this user class
+     */
 	public TravellerType getTravellerType() {
 		return travellerType;
 	}
 
-/**
- * Get the id of this user class
- * 
- * @return        id of this user class
- */
+    /**
+     * Get the id of this user class
+     * 
+     * @return        id of this user class
+     */
 	public long getId() {
 		return id;
 	}
 
-/**
- * Get the name of this user class
- * 
- * @return     the name of this user class
- */
+    /**
+     * Get the name of this user class
+     * 
+     * @return     the name of this user class
+     */
 	public String getName() {
 		return name;
 	}
 	
-/**
- * Return the mode of travel of this user class
- * 
- * @return       Mode of this user class
- */
+    /**
+     * Return the mode of travel of this user class
+     * 
+     * @return       Mode of this user class
+     */
 	public Mode getMode() {
 		return mode;
-	}
-
-/**
- * Return the id of the mode of this user class
- * 
- * @return         id of the mode of this user class
- */
-	public long getModeId() {
-		return modeId;
-	}
-
-/**
- * Return the id of the traveller type of this user class
- * 
- * @return           id of the traveller type of this user class
- */
-	public long getTravellerTypeId() {
-		return travellerTypeId;
 	}
 
 }
