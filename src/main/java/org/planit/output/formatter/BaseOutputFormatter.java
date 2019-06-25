d460dc48050ac89cbb5aba0cf4b83047f8383fff
Gary Mann <g.mann@sydney.edu.au>
2019-06-25 17:49:13 +1000

    Changes in BaseOutputFormatter to read <version> and <description> elements from pom.xml

--- src/main/java/org/planit/output/formatter/BaseOutputFormatter.java
+++ src/main/java/org/planit/output/formatter/BaseOutputFormatter.java
@@ -1,5 +1,13 @@
 package org.planit.output.formatter;
 
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import org.planit.exceptions.PlanItException;
 import org.planit.output.OutputManager;
 import org.planit.utils.IdGenerator;
 
@@ -12,22 +20,78 @@ import org.planit.utils.IdGenerator;
  */
 public abstract class BaseOutputFormatter implements OutputFormatter {
 
-    /**
-     * Unique internal id of the output writer
-     */
-    protected long id;
+	/**
+	 * Unique internal id of the output writer
+	 */
+	protected long id;
 
-    /**
-     * Base constructor
-     */
-    public BaseOutputFormatter() {
-        this.id = IdGenerator.generateId(OutputManager.class);
-    }
+	/**
+	 * Description to be included in the XML file in the <description> element
+	 */
+	protected String description;
 
-    // getters - setters
+	/**
+	 * Version to be included in the XML file in the <version> element
+	 */
+	protected String version;
 
-    public long getId() {
-        return id;
-    }
+	/**
+	 * Base constructor
+	 * 
+	 * @throws XmlPullParserException
+	 * @throws IOException
+	 * @throws FileNotFoundException
+	 */
+	public BaseOutputFormatter() throws PlanItException {
+		//try {
+			this.id = IdGenerator.generateId(OutputManager.class);
+			try (InputStream input = BaseOutputFormatter.class.getClassLoader().getResourceAsStream("application.properties")) {
+
+	            Properties prop = new Properties();
+
+	            if (input == null) {
+	                System.out.println("Sorry, unable to find application.properties");
+	                return;
+	            }
+
+	            //load a properties file from class path, inside static method
+	            prop.load(input);
+
+	            description = prop.getProperty("planit.description");
+	            version = prop.getProperty("planit.version");
+
+		} catch (Exception e) {
+			throw new PlanItException(e);
+		}
+
+	}
+
+	// getters - setters
+
+	public long getId() {
+		return id;
+	}
+
+	/**
+	 * Allows the developer to set the String to be used in the <description> element in the output XML file
+	 * 
+	 * If this method is not called, the contents of the <description> element in the PlanIt POM file is used.
+	 * 
+	 * @param description description to be included
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * Allows the developer to set the String to be used in the <version> element in the output XML file
+	 * 
+	 * If this method is not called, the contents of the <version> element in the PlanIt POM file is used.
+	 * 
+	 * @param version version to be included
+	 */
+	public void setVersion(String version) {
+		this.version = version;
+	}
 
 }
