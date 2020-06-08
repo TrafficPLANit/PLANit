d7b3f8cb4b7ea750fb4fc65aa4427517a006bd68
Mark Raadsen <mark.raadsen@sydney.edu.au>
2020-06-08 10:07:26 +1000

    #22 on PLANitIO also required nameRoot (description) of runs to be without spaces. Added replace call to achieve this on the nameRoot string when persisting.

--- src/main/java/org/planit/output/formatter/FileOutputFormatter.java
+++ src/main/java/org/planit/output/formatter/FileOutputFormatter.java
@@ -2,6 +2,7 @@ package org.planit.output.formatter;
 
 import java.io.File;
 import java.nio.file.Files;
+
 import org.planit.exceptions.PlanItException;
 import org.planit.output.enums.OutputType;
 import org.planit.time.TimePeriod;
@@ -14,60 +15,68 @@ import org.planit.time.TimePeriod;
  */
 public abstract class FileOutputFormatter extends BaseOutputFormatter {
 
+  /**
+   * Generates the name of an output file. All output files have no spaces in
+   * them.
+   * 
+   * @param outputDirectory location output files are to be written
+   * @param nameRoot        root name of the output files
+   * @param nameExtension   extension of the output files
+   * @param timePeriod      the time period
+   * @param outputType      the OutputType of the output
+   * @param runId           the id of the traffic assignment run
+   * @param iteration       current iteration
+   * @return the name of the output file
+   * @throws PlanItException thrown if the output directory cannot be opened
+   */
+  protected String generateOutputFileName(String outputDirectory, String nameRoot, String nameExtension, TimePeriod timePeriod, OutputType outputType, long runId, int iteration)
+      throws PlanItException {
+    try {
+      File directory = new File(outputDirectory);
+      if (!directory.isDirectory()) {
+        Files.createDirectories(directory.toPath());
+      }
 
-	/**
-	 * Generates the name of an output file.
-	 * 
-	 * @param outputDirectory location output files are to be written
-	 * @param nameRoot        root name of the output files
-	 * @param nameExtension   extension of the output files
-	 * @param timePeriod the time period
-	 * @param outputType the OutputType of the output
-	 * @param runId the id of the traffic assignment run
-	 * @param iteration       current iteration
-	 * @return the name of the output file
-	 * @throws PlanItException thrown if the output directory cannot be opened
-	 */
-	protected String generateOutputFileName(String outputDirectory, String nameRoot, String nameExtension, TimePeriod timePeriod, OutputType outputType, long runId, int iteration) throws PlanItException {
-		try {
-			File directory = new File(outputDirectory);
-			if (!directory.isDirectory()) {
-				Files.createDirectories(directory.toPath());
-			}
-			String newFileName = null;
-			if (timePeriod == null) {
-				if (iteration ==  -1) {
-					newFileName = outputDirectory + "\\" + outputType.value() + "_RunId_" + runId + "_" + nameRoot + nameExtension; 
-				} else {
-					newFileName = outputDirectory + "\\" + outputType.value() + "_RunId_" + runId +  "_" + nameRoot + "_" + iteration + nameExtension; 
-				}
-			} else {
-				if (iteration ==  -1) {
-					newFileName = outputDirectory + "\\" + outputType.value() + "_RunId_" + runId + "_" + nameRoot + "_" + timePeriod.getDescription().replace(' ','_') + nameExtension;
-				} else {
-					newFileName = outputDirectory + "\\" + outputType.value() + "_RunId_" + runId + "_" + nameRoot + "_" + timePeriod.getDescription().replace(' ','_') + "_" + iteration	+ nameExtension;
-				}
-			} 
-			return newFileName;
-		} catch (Exception e) {
-			throw new PlanItException(e);
-		}
-	}
-	
-	/**
-	 * Generates the name of an output file.
-	 * 
-	 * @param outputDirectory location output files are to be written
-	 * @param nameRoot        root name of the output files
-	 * @param nameExtension   extension of the output files
-	 * @param timePeriod timePeriod
-	 * @param outputType the OutputType of the output
-	 * @param runId the id of the traffic assignment run
-	 * @return the name of the output file
-	 * @throws PlanItException thrown if the output directory cannot be opened
-	 */
-	protected String generateOutputFileName(String outputDirectory, String nameRoot, String nameExtension, TimePeriod timePeriod, OutputType outputType, long runId) throws PlanItException {
-		return generateOutputFileName(outputDirectory, nameRoot, nameExtension, timePeriod, outputType, runId, -1);
-	}
+      // make sure all spaces are removed from result file
+      String nameRootNoSpace = nameRoot.replaceAll(" ", "_");
+
+      String newFileName = null;
+      if (timePeriod == null) {
+        if (iteration == -1) {
+          newFileName = outputDirectory + "\\" + outputType.value() + "_RunId_" + runId + "_" + nameRootNoSpace + nameExtension;
+        } else {
+          newFileName = outputDirectory + "\\" + outputType.value() + "_RunId_" + runId + "_" + nameRootNoSpace + "_" + iteration + nameExtension;
+        }
+      } else {
+        if (iteration == -1) {
+          newFileName = outputDirectory + "\\" + outputType.value() + "_RunId_" + runId + "_" + nameRootNoSpace + "_" + timePeriod.getDescription().replace(' ', '_')
+              + nameExtension;
+        } else {
+          newFileName = outputDirectory + "\\" + outputType.value() + "_RunId_" + runId + "_" + nameRootNoSpace + "_" + timePeriod.getDescription().replace(' ', '_') + "_"
+              + iteration + nameExtension;
+        }
+      }
+      return newFileName;
+    } catch (Exception e) {
+      throw new PlanItException(e);
+    }
+  }
+
+  /**
+   * Generates the name of an output file.
+   * 
+   * @param outputDirectory location output files are to be written
+   * @param nameRoot        root name of the output files
+   * @param nameExtension   extension of the output files
+   * @param timePeriod      timePeriod
+   * @param outputType      the OutputType of the output
+   * @param runId           the id of the traffic assignment run
+   * @return the name of the output file
+   * @throws PlanItException thrown if the output directory cannot be opened
+   */
+  protected String generateOutputFileName(String outputDirectory, String nameRoot, String nameExtension, TimePeriod timePeriod, OutputType outputType, long runId)
+      throws PlanItException {
+    return generateOutputFileName(outputDirectory, nameRoot, nameExtension, timePeriod, outputType, runId, -1);
+  }
 
 }
