84134884db02c80a9e025e116394526116fcd739
markr <mark.raadsen@sydney.edu.au>
2024-02-16 16:43:38 +1100

    #105 refinements to make it more readable

--- src/main/java/org/goplanit/algorithms/shortest/ShortestPathKShortestYen.java
+++ src/main/java/org/goplanit/algorithms/shortest/ShortestPathKShortestYen.java
@@ -52,6 +52,43 @@ public class ShortestPathKShortestYen implements ShortestPathOneToOne{
   }
 
   /**
+   * Find and mark all exit segments as excluded for all k-2-shortest paths that share the root path of the k-1 shortest
+   * path up until the provided forkNode index. These exit segments will be added to the to be provided excludedLinksToPopulate
+   *
+   * @param excludedLinksToPopulate add excluded link segments to this container
+   * @param forkNodeIndex index of node up until it is expected that the rootpath coincides with the most recent k-shortest path in kShortestRawPathsWithCost
+   * @param kShortestRawPathsWithCost the so far found k-shortest paths of which the last entry is used as reference to identify rootpaths
+   * @param excludedLinksThreshold break condition upon which we terminate search if we reach threshold (inclusive, e.g., when container has x entries and threshold is x we stop)
+   */
+  private static void excludeExitLinkSegmentsFromCoincidingRootPaths(
+          TreeSet<EdgeSegment> excludedLinksToPopulate, int forkNodeIndex, ArrayList<Pair<Deque<EdgeSegment>, Double>> kShortestRawPathsWithCost, int excludedLinksThreshold) {
+    var lastestKShortestPath = kShortestRawPathsWithCost.get(kShortestRawPathsWithCost.size()-1).first();
+    int numFoundKShortestPaths = kShortestRawPathsWithCost.size();
+    for(int p = 0; p < numFoundKShortestPaths-1 ; ++p){
+      var pathP = kShortestRawPathsWithCost.get(p).first();
+      var pathPIterUpToFork = pathP.iterator();
+      EdgeSegment pathPIterCurrSegment = pathPIterUpToFork.next();
+      var kMin1IterUpToFork = lastestKShortestPath.iterator();
+      int indexUpToForkNode = 0;
+      while(indexUpToForkNode < forkNodeIndex){
+        if(pathPIterUpToFork.hasNext() && !(kMin1IterUpToFork.next().idEquals(pathPIterCurrSegment))){
+          break;
+        }
+        ++indexUpToForkNode;
+        pathPIterCurrSegment = pathPIterUpToFork.next();
+      }
+      boolean coincidesUpToForkNode = indexUpToForkNode==forkNodeIndex;
+      if(coincidesUpToForkNode && pathPIterUpToFork.hasNext()){
+        /* exclude the link adjacent (exiting) the fork node on the path for our current potential shortest path  */
+        excludedLinksToPopulate.add(pathPIterCurrSegment);
+        if(excludedLinksToPopulate.size() >= excludedLinksThreshold){
+          break; // pre-emptive check in case we know we can break already (because all potential exist segments are excluded for this fork)
+        }
+      }
+    }
+  }
+
+  /**
    * Constructor for an edge cost based Dijkstra algorithm for finding shortest paths.
    *
    * @param oneToAllBootStrapSearch per origin, use this once to construct one-to-all wide shortest paths most efficiently
@@ -101,20 +138,22 @@ public class ShortestPathKShortestYen implements ShortestPathOneToOne{
       oneToAll = oneToAllBootStrapSearch.executeOneToAll(origin);
     }
 
-    // track k-shortest paths found and their associated cost
+    /* k-shortest (raw) paths found so far and their associated cost + shortest path as initial entry*/
     var kShortestRawPathsWithCost = new ArrayList<Pair<Deque<EdgeSegment>,Double>>(numShortestPaths);
-    // track potential candidates for a particular k-shortest path by their cost (when multiple entries with the same cost exist track all options)
-    // we also keep track of the fork node, i.e, location where it branched off from the previous shortest path to avoid unnecessary regeneration
-    // of paths
-    var potentialShortestPathAlternatives = new TreeMap<Double, List<Pair<DirectedVertex, Deque<EdgeSegment>>>>(Comparator.naturalOrder());
-
-    var upstreamForkNodeIndex = new ArrayList<Integer>(numShortestPaths);
     kShortestRawPathsWithCost.add(Pair.of(oneToAll.createRawPath(origin, destination), oneToAll.getCostToReach(destination)));
     DirectedVertex kMinus1ForkNode = origin;
+    var excludedLinks = new TreeSet<EdgeSegment>();
+
+    /* track potential candidates found when doing a pass along the most recent found k-shortest path by eliminating single
+       links on that path from being used to find other paths which may or may not be the next shortest.
+
+       we also keep track of the fork node of each potential path, i.e, location where it branched off from the previous
+       shortest path to avoid unnecessary regeneration of already identified potential shortest paths during previous passes
+     */
+    var potentialShortestPathAlternatives = new TreeMap<Double, List<Pair<DirectedVertex, Deque<EdgeSegment>>>>(Comparator.naturalOrder());
     for(int k = 1; k<numShortestPaths; ++k){
 
-      // find all fork nodes for the current k-1 shortest path as a starting point for finding the next possible alternative
-      // shortest path
+      /* select previous k-shortest path as reference point */
       final var kMinus1ShortestPath = kShortestRawPathsWithCost.get(k-1).first();
       final int kMinus1PathSize = kMinus1ShortestPath.size();
 
@@ -123,27 +162,29 @@ public class ShortestPathKShortestYen implements ShortestPathOneToOne{
       // eligible anyway
       var kMinus1ShortestPathIter = kMinus1ShortestPath.iterator();
 
-      var altShortestPath = new LinkedList<EdgeSegment>();
-      double currAltPathCost = 0;
+      /* prep */
+      var currPotentialShortestPath = new LinkedList<EdgeSegment>();
+      double currPotentialPathCostUpToFork = 0;
 
-      var excludedLinks = new TreeSet<EdgeSegment>();
-
-      EdgeSegment prevForkSegment = null;
-      EdgeSegment currForkSegment = null;
+      EdgeSegment prevForkSegment = null, currForkSegment = null;
       boolean reachedKMinus1ForkNode = false;
       for(int forkNodeIndex = 0; forkNodeIndex < kMinus1PathSize-1; ++forkNodeIndex){
+
+        /* track cost up to the current chosen fork node so we can reconstruct full alternative path cost once we have it */
         prevForkSegment = currForkSegment;
         currForkSegment = kMinus1ShortestPathIter.next();
         var currForkNode = currForkSegment.getUpstreamVertex();
 
         if(prevForkSegment != null) {
-          altShortestPath.add(prevForkSegment);
-          currAltPathCost += getEdgeSegmentCost(prevForkSegment);
+          currPotentialShortestPath.add(prevForkSegment);
+          currPotentialPathCostUpToFork += getEdgeSegmentCost(prevForkSegment);
         }
 
-        // skip finding alternative paths until we reached the fork node of the k-1 shortest path (in relation to k-2),
-        // since up until that fork node, alternatives have already been added because those links were part of k-2 which has
-        // already been exhausted for options present in the potential pool of paths
+        /* skip any attempt to find alternative path using costly one-to-one path search until we reached the fork node
+           of the k-1 shortest path (in relation to k-2) since up until that fork node, alternatives have already been
+           added to the potential shortest paths because those links were part of k-2 shortest path which - when it was being constructed
+           already identified all possible options by forking of that root path up to the fork
+         */
         // todo: this could be made smarter as we now only track in relation to previous rootpath, but we could identify any coincidence with
         //       any rootpoth for found paths 0-(k-1) of course.
         if(currForkNode.idEquals(kMinus1ForkNode)) {
@@ -153,62 +194,42 @@ public class ShortestPathKShortestYen implements ShortestPathOneToOne{
           continue;
         }
 
+        /* only consider fork node if it has exit links */
         var numForkNodeExitSegments = currForkNode.getNumberOfExitEdgeSegments();
         int numExcludedForkExitSegments = (prevForkSegment!= null && prevForkSegment.hasOppositeDirectionSegment()) ? 2 : 1;
         if(numForkNodeExitSegments - numExcludedForkExitSegments <= 0){
-          // no options to fork at this node, so skip finding alternative path and move to next node
           continue;
         }
 
         /****************** EXCLUDE non-eligible exit segments *****************************************************************/
-        // excluding already used exit edges of forknode  from previously found shortest paths that coincided up to the fork node
         excludedLinks.clear();
-        if(bannedSegments != null){
-          excludedLinks.addAll(bannedSegments);
-        }
-        // 0. do not allow loops or reusing of edges, instead of adding all link sup to here, just add preceding one which has the same effect in practice
-        //    yet is computationally more efficient
-        if(prevForkSegment != null){
-          var prevKMinus1OppositeDirectionLink = prevForkSegment.getOppositeDirectionSegment();
-          if(prevKMinus1OppositeDirectionLink != null) {
-            excludedLinks.add(prevKMinus1OppositeDirectionLink);
+        {
+          // banned segments are always excluded
+          if(bannedSegments != null){
+            excludedLinks.addAll(bannedSegments);
           }
-        }
-        // 1. the one from the k-1 shortest path exiting the fork node, so this is not an option for the alternative
-        excludedLinks.add(currForkSegment);
-
-        // 2. make sure we also exclude all outgoing edges used in previous found shortest paths (other than k-1)
-        // that shared the same paths up to the fork node, so we do not keep finding an earlier found alternative
-        //todo: this could be simpler if we track how the k-1 relates to k-2 in terms of until what link it overlaps,
-        // then we can use this to figure out if we need to add an excluded exit link, rather than traversing the entire path
-        // over and over
-        for(int p = 0; p < k-1 ; ++p){
-          var pathP = kShortestRawPathsWithCost.get(p).first();
-          var pathPIterUpToFork = pathP.iterator();
-          EdgeSegment pathPIterCurrSegment = pathPIterUpToFork.next();
-          var kMin1IterUpToFork = kMinus1ShortestPath.iterator();
-          int indexUpToForkNode = 0;
-          while(indexUpToForkNode < forkNodeIndex){
-            if(pathPIterUpToFork.hasNext() && !(kMin1IterUpToFork.next().idEquals(pathPIterCurrSegment))){
-              break;
-            }
-            ++indexUpToForkNode;
-            pathPIterCurrSegment = pathPIterUpToFork.next();
-          }
-          boolean coincidesUpToForkNode = indexUpToForkNode==forkNodeIndex;
-          if(coincidesUpToForkNode && pathPIterUpToFork.hasNext()){
-            /* exclude the link adjacent (exiting) the fork node on the path for our current potential shortest path  */
-            excludedLinks.add(pathPIterCurrSegment);
-            ++numExcludedForkExitSegments;
-            if(numForkNodeExitSegments - numExcludedForkExitSegments <= 0){
-              break; // pre-emptive check in case we know we can break already because all potential exist segments are excluded for this fork
+          // 0. do not allow loops or reusing of edges, instead of adding all link sup to here, just add preceding one which has the same effect in practice
+          //    yet is computationally more efficient
+          if(prevForkSegment != null){
+            var prevKMinus1OppositeDirectionLink = prevForkSegment.getOppositeDirectionSegment();
+            if(prevKMinus1OppositeDirectionLink != null) {
+              excludedLinks.add(prevKMinus1OppositeDirectionLink);
             }
           }
-        }
+          // 1. the one from the k-1 shortest path exiting the fork node, so this is not an option for the alternative
+          excludedLinks.add(currForkSegment);
 
-        if(numForkNodeExitSegments - numExcludedForkExitSegments <= 0){
-          // no options remaining to fork at this node, so skip finding alternative path and move to next node
-          continue;
+          // 2. make sure we also exclude all outgoing edges used in previous found shortest paths (other than k-1)
+          // that shared the same paths up to the fork node, so we do not keep finding an earlier found alternative
+          //todo: this could be simpler if we track how the k-1 relates to k-2 in terms of until what link it overlaps,
+          // then we can use this to figure out if we need to add an excluded exit link, rather than traversing the entire path
+          // over and over
+          int excludedLinksThreshold = excludedLinks.size() + (numForkNodeExitSegments-numExcludedForkExitSegments);
+          excludeExitLinkSegmentsFromCoincidingRootPaths(excludedLinks, forkNodeIndex, kShortestRawPathsWithCost, excludedLinksThreshold);
+          if(excludedLinksThreshold == excludedLinks.size()){
+            // no options remaining to fork at this node, so skip finding alternative path and move to next node
+            continue;
+          }
         }
 
         /****************** DO ONE-TO-ONE ALTERNATIVE PATH SEARCH *****************************************************************/
@@ -218,10 +239,11 @@ public class ShortestPathKShortestYen implements ShortestPathOneToOne{
           continue;
         }
 
-        /**************** CONSTRUCT POTENTIAL K_SHORTEST PATH *** *****************************************************************/
-        var potentialKShortestPath = new LinkedList<>(altShortestPath);
+        /**************** CONSTRUCT NEW POTENTIAL K_SHORTEST PATH *** *****************************************************************/
+        var potentialKShortestPath = new LinkedList<>(currPotentialShortestPath);
         potentialKShortestPath.addAll(oneToOneRawPath);
-        double potentialKShortestPathCost = currAltPathCost + oneToOnePerOriginResult.getCostToReach(destination); // cost from fork to destination
+        /* combine cost up to fork + fork to destination to get full path cost */
+        double potentialKShortestPathCost = currPotentialPathCostUpToFork + oneToOnePerOriginResult.getCostToReach(destination);
         if(!potentialShortestPathAlternatives.containsKey(potentialKShortestPathCost)){
           potentialShortestPathAlternatives.put(potentialKShortestPathCost, new ArrayList<>(2));
         }
@@ -232,7 +254,7 @@ public class ShortestPathKShortestYen implements ShortestPathOneToOne{
       if(potentialShortestPathAlternatives.isEmpty()){
         break;
       }
-      // select and remove from remaining options + update offset cost up to fork node for next iteration
+      /* Select cheapest, remove it from candidates, track fork node used so we can skip some unneeded searches in next round */
       Deque<EdgeSegment> foundKShortestPath = null;
       var foundKShortestPathWithEqualMinCostEntry = potentialShortestPathAlternatives.firstEntry();
       var foundKShortestPathsWithEqualMinCost = foundKShortestPathWithEqualMinCostEntry.getValue();
